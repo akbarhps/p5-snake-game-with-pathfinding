666c497fd8f908da2408834d63beb5d35b87ebd8
Akbarputra <akbarputrasrg@gmail.com>
2021-08-28 14:05:11 +0700

    add frameRate input

--- sketch.js
+++ sketch.js
@@ -1,37 +1,23 @@
 let canvasSize;
+let cellSize;
 
 let grid = [];
 let gridRow = 20;
 let gridColumn = 20;
 
-let cellSize;
-
 let foodCell;
 let snakeBody = [];
 let bfsMoves = [];
 
-function setupLayout() {
+function setup() {
+    frameRate(10);
     canvasSize = min(windowHeight, windowWidth);
+    canvasSize -= (canvasSize * 0.1);
     createCanvas(canvasSize, canvasSize).parent('view');
     cellSize = Math.floor(canvasSize / gridRow);
 
-    for (let y = 0; y < gridRow; y++) {
-        let cols = [];
-        for (let x = 0; x < gridColumn; x++) {
-            const cell = new Cell(x, y);
-            cols.push(cell);
-            cell.draw();
-        }
-        grid.push(cols);
-    }
-
-    foodCell = grid[Math.floor(random(gridRow))][Math.floor(random(gridColumn))];
-    snakeBody = [grid[Math.floor(random(gridRow))][Math.floor(random(gridColumn))]];
-}
-
-function setup() {
-    frameRate(15);
-    setupLayout();
+    resetProgress();
+    frameRateListener();
 }
 
 function draw() {
@@ -51,6 +37,11 @@ function bfs() {
         let nextSet = [];
         for (let cell of currentSet) {
             nextSet.push(...cell.findNeighbors());
+        }
+
+        if (nextSet.length === 0) {
+            resetProgress();
+            return;
         }
 
         currentSet = nextSet;
@@ -88,16 +79,18 @@ function resetGridVisited() {
 }
 
 function updateSnake() {
-    const snakeLength = snakeBody.length;
-    const tail = snakeBody[snakeLength - 1];
+    const tail = snakeBody[snakeBody.length - 1];
     const head = bfsMoves[0];
 
     bfsMoves.shift();
     snakeBody.unshift(head);
+    head.isBody = true;
+
     if (head === foodCell) {
         generateFood();
     } else {
         tail.draw();
+        tail.isBody = false;
         snakeBody.pop();
     }
 
@@ -105,7 +98,41 @@ function updateSnake() {
 }
 
 function generateFood() {
-    let randX = Math.floor(random(0, gridColumn));
-    let randY = Math.floor(random(0, gridRow));
+    let randX = -1, randY = -1;
+    while (randX < 0 || randY < 0 || grid[randY][randX].isBody) {
+        randY = Math.floor(random(0, gridRow));
+        randX = Math.floor(random(0, gridColumn));
+    }
     foodCell = grid[randY][randX];
+}
+
+function frameRateListener() {
+    let sizeInput = document.getElementById('frameRate');
+    sizeInput.addEventListener('input', (event) => {
+        let value = Number(event.target.value);
+        if (value < 10) {
+            value = 10;
+        } else if (value > 60) {
+            value = 60;
+        }
+        frameRate(value);
+    });
+}
+
+function resetProgress() {
+    grid = [];
+    for (let y = 0; y < gridRow; y++) {
+        let cols = [];
+        for (let x = 0; x < gridColumn; x++) {
+            const cell = new Cell(x, y);
+            cols.push(cell);
+            cell.draw();
+        }
+        grid.push(cols);
+    }
+
+    bfsMoves = [];
+    foodCell = grid[Math.floor(random(gridRow))][Math.floor(random(gridColumn))];
+    snakeBody = [grid[Math.floor(random(gridRow))][Math.floor(random(gridColumn))]];
+    snakeBody[0].isBody = true;
 }
